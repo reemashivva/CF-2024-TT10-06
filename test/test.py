cbc817bf2f77dd224142198bfe2713ebfa07d958
reemashivva <127686007+reemashivva@users.noreply.github.com>
2025-02-21 22:06:57 +0530

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,40 +1,49 @@
-# SPDX-FileCopyrightText: © 2024 Tiny Tapeout
-# SPDX-License-Identifier: Apache-2.0
-
 import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import ClockCycles
 
-
 @cocotb.test()
 async def test_project(dut):
-    dut._log.info("Start")
+  dut._log.info("Start")
+  write = dut.ui_in[2]
+  read = dut.ui_in[3]
+  write_data = [
+    dut.ui_in[4],
+    dut.ui_in[5],
+    dut.ui_in[6],
+    dut.ui_in[7]
+  ]
+  empty = dut.uo_out[1]
+  full = dut.uo_out[0]
+  read_data = [
+    dut.uo_out[2],
+    dut.uo_out[3],
+    dut.uo_out[4],
+    dut.uo_out[5]
+  ]
+  reset = dut.rst_n
+  write.value = 0
+  read.value = 0
+  for i in write_data:
+    i.value = 0
 
-    # Set the clock period to 10 us (100 KHz)
-    clock = Clock(dut.clk, 10, units="us")
-    cocotb.start_soon(clock.start())
+  clock = Clock(dut.clk, 5, units="us")
+  cocotb.start_soon(clock.start())
+  
+  # Reset
+  dut._log.info("Reset")
+  reset.value = 1
+  await ClockCycles(dut.clk, 10)
+  reset.value = 0
+  await ClockCycles(dut.clk, 10)
 
-    # Reset
-    dut._log.info("Reset")
-    dut.ena.value = 1
-    dut.ui_in.value = 0
-    dut.uio_in.value = 0
-    dut.rst_n.value = 0
-    await ClockCycles(dut.clk, 10)
-    dut.rst_n.value = 1
+  # Set the input values, wait one clock cycle, and check the output
+  dut._log.info("Test")
+  assert empty.value == 1
+  assert full.value == 0
 
-    dut._log.info("Test project behavior")
-
-    # Set the input values you want to test
-    dut.ui_in.value = 0
-    dut.uio_in.value = 0
-
-    # Wait for one clock cycle to see the output values
-    await ClockCycles(dut.clk, 1)
-
-    # The following assersion is just an example of how to check the output values.
-    # Change it to match the actual expected output of your module:
-    assert dut.uo_out.value == 0
-
-    # Keep testing the module by changing the input values, waiting for
-    # one or more clock cycles, and asserting the expected output values.
+  write.value = 1
+  await ClockCycles(dut.clk, 2)
+  write.value = 0
+  await ClockCycles(dut.clk, 10)
+  assert empty.value == 0
